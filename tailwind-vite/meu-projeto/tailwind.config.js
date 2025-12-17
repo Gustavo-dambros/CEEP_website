d3f16c03e0416997bebd4ec0ce546ebfa3d9b1af
Gustavo Dambros <gustavodambros.19@gmail.com>
2025-12-17 16:00:17 -0300

    footer

--- tailwind-vite/meu-projeto/tailwind.config.js
+++ tailwind-vite/meu-projeto/tailwind.config.js
@@ -4,7 +4,9 @@ export default {
       colors: {
         primaria: '#ff0000',
         ceep_green: '#0FB700',
+        ceep_black_green:'##0C8F00',
         ceep_blue: '#080073',
+        ceep_black_blue:'#060061',
       },
     },
   },
