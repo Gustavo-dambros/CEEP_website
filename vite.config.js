3569058de7093347b06c18049673349932b4b807
Gustavo-dambros <gustavodambros.19@gmail.com>
2026-01-09 08:25:26 -0300

    tentando por mais pagina no gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/CEEP_website', // relativo ao index.html
-  plugins: [tailwindcss()]
+  base: '/CEEP_website/',
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        'adm-info': './adm-info.html',
+      },
+    },
+  },
 })
