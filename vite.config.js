acbcbea29f7275bd28d56a7390d3f36dae766976
Gustavo-dambros <gustavodambros.19@gmail.com>
2026-01-14 10:57:06 -0300

    pagina de "nossos cursos"

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: './index.html',
+        'cursos': 'cursos.html',
         'administracao': 'src/cursos/administracao.html',
         'analise_e_desenvolvimento': 'src/cursos/ads.html',
         'edificacoes': 'src/cursos/edificacoes.html',
