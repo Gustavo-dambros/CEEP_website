7118fe4641eb05c95e222ec3993b15559e0e1cba
Gustavo Dambros <gustavodambros.19@gmail.com>
2026-01-16 15:36:54 -0300

    botao

--- src/javascript/ads.js
+++ src/javascript/ads.js
@@ -10,18 +10,28 @@ let estrutura = document.getElementById("estrutura")
 
 
 Aatuacao.addEventListener("click", () => {
+  Aatuacao.classList.toggle("bg-gray-400")
+   conhecimento.classList.remove("bg-gray-400")
+  estrutura.classList.remove("bg-gray-400")
     txt_atuacao.classList.toggle("hidden")
     txt_conhecimento.classList.add("hidden")
     txt_estrutura.classList.add("hidden")
   })
 
   conhecimento.addEventListener("click", () => {
+     conhecimento.classList.toggle("bg-gray-400")
+      Aatuacao.classList.remove("bg-gray-400")
+      estrutura.classList.remove("bg-gray-400")
     txt_atuacao.classList.add("hidden")
     txt_conhecimento.classList.toggle("hidden")
     txt_estrutura.classList.add("hidden")
   })
 
   estrutura.addEventListener("click", () => {
+    estrutura.classList.toggle("bg-gray-400")
+     Aatuacao.classList.remove("bg-gray-400")
+    conhecimento.classList.remove("bg-gray-400")
+
     txt_atuacao.classList.add("hidden")
     txt_estrutura.classList.toggle("hidden")
     txt_conhecimento.classList.add("hidden")
